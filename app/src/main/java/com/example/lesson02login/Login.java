3e9aa4c16175cf9854bee90648bedc8e6ce16777
Do Truong Giang <giang123b@gmail.com>
2019-07-21 22:51:20 +0700

    Initial commit

---
+++ app/src/main/java/com/example/lesson02login/Login.java
@@ -0,0 +1,21 @@
+package com.example.lesson02login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        getSupportActionBar().hide();
+    }
+    public void signIn(View view){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
